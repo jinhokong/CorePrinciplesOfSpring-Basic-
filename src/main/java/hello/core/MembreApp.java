866557f49bddb31173de1dc3fb28641369d7af8a
kong.jino <kong.jino@kakaoenterprise.com>
2021-03-14 21:21:48 +0900

    appConfig 추가 및 관심사 분리

--- src/main/java/hello/core/MembreApp.java
+++ src/main/java/hello/core/MembreApp.java
@@ -2,13 +2,13 @@ package hello.core;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import hello.core.member.MemberSerivceImpl;
 import hello.core.member.MemberService;
 
 public class MembreApp {
-    public static void main(String[] args){
-        MemberService memberService = new MemberSerivceImpl();
-        Member member = new Member(1L,"memberA", Grade.VIP);
+    public static void main(String[] args) {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
